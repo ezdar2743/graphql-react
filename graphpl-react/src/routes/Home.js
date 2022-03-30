3961c9bf6e3cb7bdbebe9abafee26809712e0d88
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-30 23:21:09 +0900

    design

--- graphpl-react/src/routes/Home.js
+++ graphpl-react/src/routes/Home.js
@@ -1,7 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
-
 import React from "react";
-const GET_MOVIES = gql`
+import styled from "styled-components";
+import Movie from "../components/Movie";
+
+const GET_MOIVES = gql`
   {
     movies {
       id
@@ -9,10 +11,61 @@ const GET_MOVIES = gql`
     }
   }
 `;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: 10px;
+`;
 const Home = () => {
-  const { loading, error, data } = useQuery(GET_MOVIES);
-  if (loading) return "Loading..";
-  if (data.movies) return data.movies.map((m) => <div key={m.id}>{m.id}</div>);
+  const { loading, data } = useQuery(GET_MOIVES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((movie) => <Movie key={movie.id} id={movie.id} />)}
+    </Container>
+  );
 };
 
 export default Home;
