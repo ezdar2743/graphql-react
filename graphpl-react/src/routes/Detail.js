e15cd9bf21f9c885d0e268839f1dcfba89613695
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-30 23:48:48 +0900

    useParams, query getMovie, useQuery variables

--- graphpl-react/src/routes/Detail.js
+++ graphpl-react/src/routes/Detail.js
@@ -1,10 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { useParams } from "react-router-dom";
 
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      id
+      medium_cover_image
+      genres
+      description_intro
+    }
+  }
+`;
+
 const Detail = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div>Detail</div>;
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+
+  if (loading) return "Loading..";
+  if (data && data.movie) return data.movie.title;
 };
 
 export default Detail;
