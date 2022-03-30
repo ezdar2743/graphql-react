22a9c1e3f8b69d1c96664f483122cbb4fab7fd64
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-30 22:50:25 +0900

    provider , gql useQuerry

--- graphpl-react/src/index.js
+++ graphpl-react/src/index.js
@@ -1,12 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Globalstyle from "./theme/theme";
+import { Globalstyle } from "./theme/global";
+import client from "./apollo";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Globalstyle />
-    <App />
+    <ApolloProvider client={client}>
+      <Globalstyle />
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- graphpl-react/src/routes/Home.js
+++ graphpl-react/src/routes/Home.js
@@ -1,6 +1,17 @@
-import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
+import React from "react";
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(data);
   return <div>Home</div>;
 };
 
